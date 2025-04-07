325fd6a9aed914d5e1fe17926473a4b56311b414
debian <debian@vbox.myguest.virtualbox.org>
2025-04-07 16:11:17 +0200

    fix: certificat link

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/certificate', (req, res) => {
+app.get('/certificat', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'certificate.html'));
 });
 
